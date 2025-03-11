41e344ec03203c0ff51d76761bf4eb639b6c1501
Guillermo Cabrera <gcabrerojas@gmail.com>
2025-03-11 01:15:50 -0300

    fix(build)

--- src/shared/decorators/collection.decorator.ts
+++ src/shared/decorators/collection.decorator.ts
@@ -1,9 +1,9 @@
 // src/shared/decorators/collection.decorator.ts
-import 'reflect-metadata';
+import "reflect-metadata";
 
 export function Collection(name: string) {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
-    return function (constructor: Function) {
-        Reflect.defineMetadata('collection', name, constructor);
-    };
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
+  return function (constructor: Function) {
+    Reflect.defineMetadata("collection", name, constructor);
+  };
 }
