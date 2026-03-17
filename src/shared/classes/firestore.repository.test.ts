36deaedae868d392a25405d0f6030717230ec130
cabrerojas <solid.dreyko@gmail.com>
2026-03-17 09:38:24 -0300

    test: add jest + basic unit tests for FirestoreRepository; jest config

--- src/shared/classes/firestore.repository.test.ts
+++ src/shared/classes/firestore.repository.test.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any, @typescript-eslint/no-unused-vars */
+
 jest.mock('@/config/firebase', () => {
   const mockCollection = () => ({
     doc: (id?: string) => ({
