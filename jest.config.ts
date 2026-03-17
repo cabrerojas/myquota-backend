05165f84f361659d3d0817b70a9e8fd0c5771ac9
cabrerojas <solid.dreyko@gmail.com>
2026-03-17 09:37:33 -0300

    test: add jest + basic unit tests for FirestoreRepository; jest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,12 +1,12 @@
-export default {
+import type { Config } from 'jest';
+
+const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  roots: ['<rootDir>/src'],
-  testMatch: ['**/__tests__/**/*.test.ts','**/*.spec.ts'],
   moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/src/$1",
-    "^@modules/(.*)$": "<rootDir>/src/modules/$1",
-    "^@shared/(.*)$": "<rootDir>/src/shared/$1",
-    "^@config/(.*)$": "<rootDir>/src/config/$1",
+    '^@/(.*)$': '<rootDir>/src/$1',
   },
+  testMatch: ['**/tests/**/*.test.ts'],
 };
+
+export default config;
